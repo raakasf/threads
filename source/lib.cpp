bb0aac0d086162d1d53ebbf7e433ce19a1cb43ad
Rakesh Sehgal <4614260+raakasf@users.noreply.github.com>
2023-05-20 19:59:15 -0400

    All building and running with cmake

--- source/lib.cpp
+++ source/lib.cpp
@@ -3,6 +3,74 @@
 #include <fmt/core.h>
 
 library::library()
-    : name {fmt::format("{}", "threads")}
+    : name{fmt::format("{}", "cmake")}
 {
+    done.reserve(k_number_of_threads);
+    reaper_thread = std::thread(&library::reaper, this);
+    // std::fill(done.begin(), done.end(), false);
+}
+
+library::~library()
+{
+    SPDLOG_INFO("library destructor");
+    if (reaper_thread.joinable())
+        reaper_thread.join();
+}
+std::mt19937 Random::seeded_eng()
+{
+    std::random_device r;
+    std::seed_seq seed{r(), r(), r(), r(), r(), r(), r(), r()};
+    return std::mt19937(seed);
+}
+
+auto Random::operator()(int a, int b)
+{
+    std::uniform_int_distribution<int> dist(a, b);
+    return dist(eng);
+}
+
+void library::watch(int i)
+{
+    Random random;
+    auto x = random(1, 9);
+    SPDLOG_INFO("watch# {} launched, now going to sleep for {}", i, x);
+    std::this_thread::sleep_for(std::chrono::milliseconds(x));
+    {
+        std::lock_guard<std::mutex> lk(cv_m);
+        q.push(i);
+    }
+    SPDLOG_INFO("notifying");
+    cv.notify_one();
+    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    SPDLOG_INFO(i);
+    // std::SPDLOG_INFO("watch thread ending " << i << std::endl;
+}
+
+void library::watch_add(const int &i)
+{
+    std::lock_guard<std::mutex> lk(cv_w);
+    watches.push_back(
+        std::thread(&library::watch, this, i));
+}
+
+void library::reaper()
+{
+    SPDLOG_INFO("reaper thread started");
+    auto count = k_number_of_threads;
+    while (count--)
+    {
+        {
+            std::unique_lock<std::mutex> lk(cv_m);
+            // SPDLOG_INFO("waiting for condition, count={}", count);
+            cv.wait(lk, [&]
+                    { return !q.empty(); });
+            SPDLOG_INFO(q.front());
+            {
+                std::lock_guard<std::mutex> lk_w(cv_w);
+                watches[q.front()].join();
+                q.pop();
+            }
+        }
+    }
+    SPDLOG_INFO("reaper thread ending");
 }
--- source/lib.hpp
+++ source/lib.hpp
@@ -1,6 +1,22 @@
 #pragma once
 
 #include <string>
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <queue>
+#include <condition_variable>
+#include <mutex>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <random>
+#include <spdlog/spdlog.h>
+
+using namespace std;
+
+const unsigned long k_number_of_threads = 100;
 
 /**
  * @brief The core implementation of the executable
@@ -10,12 +26,38 @@
  * test the implementation for the executable, because the logic is nicely
  * separated from the command-line logic implemented in the main function.
  */
-struct library
+class library
 {
   /**
    * @brief Simply initializes the name member to the name of the project
    */
+  public: 
   library();
+  ~library();
+  void watch(int i);
+  void watch_add(const int& i);
+  void reaper();
+
+private:
+  std::mutex cv_w;
+  vector<thread> watches;
+  std::thread reaper_thread;
+  condition_variable cv;
+  std::mutex cv_m;
+  queue<int> q;
+
+  std::mutex cv_done;
+  vector<bool> done;
 
   std::string name;
+};
+
+
+class Random
+{
+  std::mt19937 seeded_eng();
+  std::mt19937 eng = seeded_eng();
+
+public:
+  auto operator()(int a, int b);
 };
