ab720cbb1574296e1a4bd335d708cad85f56483b
Rakesh Sehgal <4614260+raakasf@users.noreply.github.com>
2023-05-20 21:09:35 -0400

    build with sanitize

---
+++ test/source/threads_test.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+// #include "threads/threads.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Name is threads", "[library]")
+{
+  // auto const exported = exported_class {};
+  // REQUIRE(std::string("threads") == exported.name());
+}
